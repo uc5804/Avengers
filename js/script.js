1b0cac93f7255e4a060dd00155fed13a88b73611
uc5804 <uc5804@naver.com>
2021-02-15 04:39:22 +0900

    init commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+
+$("main section article").hover(function(){
+    $(this).addClass("active");
+    $(this).addClass("active");
+
+}, function(){
+    $(this).removeClass("active");
+    $(this).removeClass("active");
+});
+
+
+
+
+
+});
+
+
